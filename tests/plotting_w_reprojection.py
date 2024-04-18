169a0a6b83bd66d21579f63cb9218e3c584b922e
xli2522 <50414011+xli2522@users.noreply.github.com>
2024-04-17 22:28:13 -0400

    basic functions for multi-order maps (scatter)

--- tests/plotting_w_reprojection.py
+++ tests/plotting_w_reprojection.py
@@ -3,12 +3,26 @@ import matplotlib.pyplot as plt
 import gracedb_public.pixView as pixView
 
 # skymap projection and visualization
-
+# Flat Resolution Sky Map
 filename_ligo = \
-r'https://gracedb.ligo.org/api/superevents/MS240410u/files/bayestar.fits.gz,1'
-skymap = pixView.get_map(filename_ligo) 
-fig = pixView.mollview( skymap, nest=False, title='Test Mollweide', 
+        r'https://gracedb.ligo.org/api/superevents/MS240410u/'+\
+        r'files/bayestar.fits.gz,1'
+
+skymap = pixView.get_map_file(filename_ligo, index='nested', validate=True) 
+fig = pixView.mollview( skymap, index='nested', title='Test Mollweide', 
                         get_fig=False, 
                         save_fig=True, 
-                        save_path='tests/Test_Mollweide.png')
+                        save_path='tests/test_Mollweide_flat.png')
+plt.close()
+
+# Multiorder Sky Map
+multiorder_ligo = \
+        r'https://gracedb.ligo.org/api/superevents/'+\
+        r'MS240410u/files/bayestar.multiorder.fits,1'
+skymap = pixView.get_map_file(multiorder_ligo, index='uniq', validate=True) 
+# skymap['UNIQ']#['UNIQ']
+fig = pixView.mollview( skymap, index='uniq', title='Test Mollweide Multiorder', 
+                        get_fig=False, 
+                        save_fig=True, 
+                        save_path='tests/test_Mollweide_multi_scatter.png')
 plt.close()
